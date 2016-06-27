046add569db94732996fee4d316884f1a2029e47
Thomas <Thomas@10.0.2.15>
2016-06-27 14:59:47 +0200

    Insertion du code de Louis et placement de commentaire sur mes classes

--- src/main.java
+++ src/main.java
@@ -2,8 +2,7 @@
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		// Voilà notre main. Nous développons en objet, donc nous appellerons nos procédures et fonctions ici.
 		menu menu1= new menu();
 	}
 
--- src/menu.java
+++ src/menu.java
@@ -1,23 +1,21 @@
 import java.util.Scanner;
-
+// Classe permettant de faire afficher le menu principal. En fonction du choix de l'utilisateur, la difficulté du jeu augmentera.
 public class menu
 {	
 
 	public menu()
 	{
-	Scanner screp = new Scanner(System.in);
-	
-	
+	Scanner screp = new Scanner(System.in); //On utilise l'objet scanner qui permet de récupérer la saisie utilisateur.
 	System.out.println("\n");
 	System.out.println("Veuillez choisir la difficulté de votre jeu");
 	System.out.println("1 - Facile");
 	System.out.println("2 - Normal");
 	System.out.println("3 - Difficile");
-	int rep = screp.nextInt();
-	switch (rep)
+	int v_rep = screp.nextInt(); //On initialise notre variable de réponse qui s'appelle v_rep
+	switch (v_rep)
 	{
 	case 1 :
-			System.out.println("Facile");
+			tab montab = new tab(); // On appelle notre objet tableau pour afficher la surface de jeu.
 			break;
 	case 2 :
 			System.out.println("Normal");
@@ -29,5 +27,6 @@ public class menu
 			menu monmenu = new menu();
 			break;
 	}
+	screp.close(); // On ferme le flux du scanner pour ne pas occuper un port pour rien.
 	}
 }
